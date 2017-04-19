40f79d86e4bd4bb7aece14307f8d0b67b5d734a9
josellano <jose_llano_95@hotmail.com>
2017-04-18 23:10:08 -0300

    Agregado del icono en la pestaña. Agregado de funcionalidades para guardar y cargar estilos de la pagina. Nota: saque el drawCucha() y no lo volvi a poner.

--- Proyecto1/jscript/stylechange.js
+++ Proyecto1/jscript/stylechange.js
@@ -1,7 +1,37 @@
-function reStyle() {
-   document.getElementById("estilo1").disabled=true;
+function reStyle(est) {
+	
+	if( est=="estilo0"){
+			document.getElementById("estilo1").disabled=true;
+			guardarEstilo("estilo0");
+	}
+	else{
+		if( est=="estilo1"){
+			document.getElementById("estilo1").setAttribute("href","css/estilo1.css");
+			guardarEstilo("estilo1");
+		}
+		else{
+			document.getElementById("estilo1").setAttribute("href","css/estilo2.css");
+			guardarEstilo("estilo2");
+		}
+			document.getElementById("estilo1").disabled=false;
+	}
 }
 
 function noStyle() {
-   document.getElementById("estilo1").disabled=false;
+   document.getElementById("estilo1").disabled=true;
+   guardarEstilo("estilo0");
+}
+
+
+function cargarEstilo() {
+	var x=localStorage.getItem("estiloGuardado");
+    if(x)
+		reStyle(x);
+	else
+		reStyle("estilo1");
+}
+
+function guardarEstilo(estilo) {
+    localStorage.setItem("estiloGuardado", estilo);
+    
 }
