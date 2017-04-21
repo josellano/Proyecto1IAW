4529bc3388cb4fa48700297dcc6c7af66f5e3b75
josellano <jose_llano_95@hotmail.com>
2017-04-21 11:21:32 -0300

    Modificacion del Estilo2. gregado de paletas de colores. Modificacion de la pestaña techo, ahora cada columna tiene 2 "atributos".

--- Proyecto1/jscript/stylechange.js
+++ Proyecto1/jscript/stylechange.js
@@ -8,13 +8,23 @@ function reStyle(est) {
 		if( est=="estilo1"){
 			document.getElementById("estilo1").setAttribute("href","css/estilo1.css");
 			guardarEstilo("estilo1");
+			cambiarPestañas(est);
 		}
 		else{
 			document.getElementById("estilo1").setAttribute("href","css/estilo2.css");
 			guardarEstilo("estilo2");
+			cambiarPestañas(est);
 		}
 			document.getElementById("estilo1").disabled=false;
 	}
+}
+
+function cambiarPestañas(est){
+	
+	if( est=="estilo1")
+		document.getElementById("tipoPestaña").className="nav nav-tabs centered";
+	else
+		document.getElementById("tipoPestaña").className="nav nav-pills centered";
 }
 
 function noStyle() {
