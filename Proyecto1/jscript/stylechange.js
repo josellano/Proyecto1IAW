de86d8000b5c249cfb6bd18eafc27cd768a9f168
Scarlato_Nahuel <nahuel.scar@gmail.com>
2017-04-23 15:40:40 -0300

    Agregado de detalles de estilos.

--- Proyecto1/jscript/stylechange.js
+++ Proyecto1/jscript/stylechange.js
@@ -9,11 +9,13 @@ function reStyle(est) {
 			document.getElementById("estilo1").setAttribute("href","css/estilo1.css");
 			guardarEstilo("estilo1");
 			cambiarPestañas(est);
+			cambiarHuesos(1);
 		}
 		else{
 			document.getElementById("estilo1").setAttribute("href","css/estilo2.css");
 			guardarEstilo("estilo2");
 			cambiarPestañas(est);
+			cambiarHuesos(2);
 		}
 			document.getElementById("estilo1").disabled=false;
 	}
@@ -44,4 +46,16 @@ function cargarEstilo() {
 function guardarEstilo(estilo) {
     localStorage.setItem("estiloGuardado", estilo);
     
+}
+
+function cambiarHuesos(est){
+	var x=document.getElementsByName("hueso");
+	if( est==1){
+		x[0].setAttribute("src","source/hueso1.png");
+		x[1].setAttribute("src","source/hueso1.png");
+	}
+	else{
+		x[0].setAttribute("src","source/bone2.png");
+		x[1].setAttribute("src","source/bone2.png");
+	}
 }
